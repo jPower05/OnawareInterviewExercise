9b12077aa4fcb4f8ddf3f9ee98a7576dd8878021
jPower05 <jamespbaaah05@gmail.com>
2019-02-13 17:27:51 +0000

    Reads in and parses Json and CSV data. Stored as Student objects

--- JPowerOnawareInterviewExercise/src/Main.java
+++ JPowerOnawareInterviewExercise/src/Main.java
@@ -1,4 +1,106 @@
-//main class  gggg
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+//main class 
 public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+
+		// take in a file
+		ArrayList<Student> students = readFile("utils/students.csv");
 		
+
+		for (Student i : students) {
+			System.out.println(i.toString());
+		}
+		// determine file type
+
+		// create a Json or CSV parser depending
+
+		// Call Json/CSV.parse()
+
+		// returns an ArrayList
+
+		// sort ArrayList by name (using StudentName Comparator)
+
+		// output that ArrayList to a file (csv)
+
+		// sort the ArrayList by grade (using Student Grade Comparator
+
+		// output that ArrayList to a file (json)
+	}
+
+	public static ArrayList readFile(String filename) throws FileNotFoundException, IOException {
+		if (filename == "") {
+			System.out.println("Empty file name");
+			return null;
+		}
+
+		if (filename.contains(".csv")) {
+			ArrayList<Student> students = new ArrayList<Student>();
+			// handling csv file
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(filename));
+				String line = br.readLine();
+				while ((line = br.readLine()) != null && !line.isEmpty()) {
+					String[] fields = line.split(",");
+					String name = fields[0];
+					String stringGrade = fields[1];
+					double grade = Double.parseDouble(stringGrade);
+					Student student = new Student(name, grade);
+					students.add(student);
+				}
+				br.close();
+
+			} catch (IOException e) {
+				System.out.println("error parsing");
+				return null;
+			}
+
+			return students;
+		}
+		if (filename.contains(".json")) {
+			//reading in json file into string
+			StringBuilder sb = new StringBuilder();
+			BufferedReader br = new BufferedReader(new FileReader(filename));
+			try {
+				String line = br.readLine();
+				while (line != null) {
+					sb.append(line);
+					sb.append("\n");
+					line = br.readLine();
+				}
+			} catch (IOException e) {
+				System.out.println("error parsing");
+				return null;
+			} finally {
+				br.close();
+			}
+			//parsing json string into ArrayList<Student>
+			String json = sb.toString();
+			System.out.println(json);
+			ObjectMapper objectMapper = new ObjectMapper();
+			ArrayList<Student> studentsJson;
+			try {
+				studentsJson = objectMapper.readValue(json, new TypeReference<ArrayList<Student>>() {
+				});
+				return studentsJson;
+			} catch (JsonParseException e) {
+				System.out.println("Json file formatted incorectly");
+				return null;
+			}
+			
+		}
+		return null;
+	}
+
 }
--- JPowerOnawareInterviewExercise/src/Student.java
+++ JPowerOnawareInterviewExercise/src/Student.java
@@ -1,4 +1,40 @@
 
 public class Student {
+	private double grade;
+	private String name;
+	
+	public Student() {
+		
+	}
+	
+	public Student(String name, double grade) {
+		this.grade = grade;
+		this.name = name;
+	}
 
+	public double getGrade() {
+		return grade;
+	}
+
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [grade=" + grade + ", name=" + name + "]";
+	}
+	
+	
 }
+
+
+
